c78fba51b08c1f55cf97cc40d607a9c1d99be936
xiaodongzhang1025 <xiaodongzhang1025@163.com>
2019-12-12 16:49:10 +0800

    add download_sdkxxx file

---
+++ download_sdk_v459.sh
@@ -0,0 +1,12 @@
+if [ -z "$1" ];then
+  echo "usage: $0 sdk_dir"
+  exit -1
+fi
+if [ -e "$1" ];then
+  rm -rf "$1"
+fi
+cur_path=`pwd`
+mkdir -p "$1"
+cd "$1" && repo init -u git@192.168.19.105:AW_SDK/manifest.git -b master -m V459_IPC.xml
+cd $cur_path
+echo "----------------The End---------------"
